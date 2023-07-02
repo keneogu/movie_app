543815193ca229cdd943bfbea477501c65a3b92d
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-02 22:32:58 +0100

    Add pagenotfound File

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IPageNotFoundProps {
+}
+
+export function PageNotFound (props: IPageNotFoundProps) {
+	return (
+		<div>
+			<h1>Page not Found</h1>
+		</div>
+	);
+}
