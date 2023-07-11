747ec83f599c2f436c8101038f0fee3057d1c7f3
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-11 16:18:22 +0100

    Add type of useState to be returned

--- src/customHook/useFetchData.tsx
+++ src/customHook/useFetchData.tsx
@@ -4,25 +4,19 @@ import { apiKey } from "../Api/apiKey";
 import { reducer } from "../reducer/Reducer";
 
 export const useFetchMovie = () => {
-	const [{data, isLoading, error}, dispatch] = useReducer(reducer, {
+	const [{ data, isLoading, error }, dispatch] = useReducer(reducer, {
 		isLoading: false
 	});
 
-	const [ term, setTerm ] = useState("superman")
-	const [ type, setType ] = useState("movie")
-
-  // let response: any;
-	// const fetchData = async() => {
-	//  response = await axios
-	// 		.get(`http://www.omdbapi.com/?apikey=${apiKey}&s=${term}&type=${type}`)
-	// 		return response.data?.Search;
-	// }
-	console.log(data)
+	const [term, setTerm] = useState<string>("superman")
+	const [type, setType] = useState<string>("movie")
 
 	useEffect(() => {
 		let ignore = false;
-		dispatch({type: 'request' })
-		axios(`http://www.omdbapi.com/?apikey=${apiKey}&s=${term}&type=${type}`).then(results => { if (!ignore) dispatch({ type: 'success', results: results?.data }); }, (error) => dispatch({type: 'failure', error}))
+		dispatch({ type: 'request' })
+		axios(`http://www.omdbapi.com/?apikey=${apiKey}&s=${term}&type=${type}`)
+		.then(results => { if (!ignore) dispatch({ type: 'success', results: results?.data }); },
+		 (error) => dispatch({ type: 'failure', error }))
 		return () => { ignore = true }
 	}, [term, type])
 
