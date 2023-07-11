06c2d25e9a332799d336aeba8b05e3abc5485cef
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-11 15:17:40 +0100

    Add link to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import {FC} from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from './pages/Home';
+import { Header } from './Header';
+import { PageNotFound } from './pages/PageNotFound';
+import { MoviesContext } from './reducer/MovieContext';
 import './App.css';
 
-function App() {
+const App: FC = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <MoviesContext>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path="*" element={<PageNotFound />}/>
+          </Routes>
+        </Router>
+        </MoviesContext>
+      </div>
   );
 }
 
