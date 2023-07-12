8018311a39429084f74d57f59b20a984b6a337f0
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-07-12 14:19:00 +0100

    Style header

---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export function Header() {
+	return (
+		<div className='flex justify-center text-center text-4xl text-white p-2 font-semibold bg-stone-700 w-full'>
+			<h1>Movie App</h1>
+		</div>
+	);
+}
